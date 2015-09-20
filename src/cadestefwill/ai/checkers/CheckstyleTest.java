e4175efe5b538bf9334f392aac697e699f25c6b6
Cade Sperlich <csperlich@gmail.com>
2015-09-19 22:49:18 -0400

    - Create Eclipse project file and folder structure. - Add .checkstyle and resources/cadestefwill - Google Checks.xml.   To use the style checker download the Checkstyle Eclipse plugin   via the Eclipse Marketplace in the Eclipse "help" drop-down menu. - Auto-formatting options in .settings/ have been set to closely   mirror the Checkstyle plug-in. - Add DS_Store to .gitignore.

---
+++ src/cadestefwill/ai/checkers/CheckstyleTest.java
@@ -0,0 +1,22 @@
+package cadestefwill.ai.checkers;
+
+public class CheckstyleTest {
+
+  int checkStyleCheck;
+
+  public CheckstyleTest() {
+    checkStyleCheck = 4;
+  }
+
+  public int getCheckStyleCheckNum() {
+    return checkStyleCheck;
+  }
+
+  public static void main(String[] args) {
+    CheckstyleTest myTest = new CheckstyleTest();
+    System.out.println(myTest.getCheckStyleCheckNum());
+    int xa = 8;
+    System.out.println(xa);
+
+  }
+}
