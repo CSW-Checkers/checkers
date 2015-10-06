5b328e21fcebe277171ffcb49750cb4ff35c1dda
Cade Sperlich <csperlich@gmail.com>
2015-10-06 00:34:44 -0400

    Minor name change to MoveGeneratorTest methods

--- test/model/MoveGeneratorTest.java
+++ test/model/MoveGeneratorTest.java
@@ -13,111 +13,6 @@ import java.util.Set;
 public class MoveGeneratorTest {
 
     @Test
-    public void testDetermineJumpMoves_MultiJumpsWithoutKings() {
-
-        List<Integer> blackPositions = Arrays.asList(6, 7, 8, 14, 15);
-        List<Integer> whitePositions = Arrays.asList(18);
-        Board board = new Board(blackPositions, whitePositions);
-
-        // expected white moves
-        Set<MoveInterface> expectedJumpMoves = new HashSet<>();
-        expectedJumpMoves.add(new MultiJump(18, 2, Arrays.asList(9), board));
-        expectedJumpMoves.add(new MultiJump(18, 2, Arrays.asList(11), board));
-        expectedJumpMoves.add(new MultiJump(18, 4, Arrays.asList(11), board));
-
-        PieceColor currenetPlayersColor = PieceColor.WHITE;
-        MoveGenerator mg = new MoveGenerator(board);
-
-        Set<MoveInterface> actualJumpMoves = mg.getJumpMoves(currenetPlayersColor);
-        assertEquals(expectedJumpMoves, actualJumpMoves);
-    }
-
-    @Test
-    public void testDetermineJumpMoves_withoutKings() {
-
-        List<Integer> blackPositions = Arrays.asList(9, 10, 11, 12);
-        List<Integer> whitePositions = Arrays.asList(13, 14, 15, 16);
-        Board board = new Board(blackPositions, whitePositions);
-
-        // expected black moves
-        Set<MoveInterface> expectedJumpMoves = new HashSet<>();
-        expectedJumpMoves.add(new SingleJump(9, 18, board));
-        expectedJumpMoves.add(new SingleJump(10, 17, board));
-        expectedJumpMoves.add(new SingleJump(10, 19, board));
-        expectedJumpMoves.add(new SingleJump(11, 18, board));
-        expectedJumpMoves.add(new SingleJump(11, 20, board));
-        expectedJumpMoves.add(new SingleJump(12, 19, board));
-
-        MoveGenerator mg = new MoveGenerator(board);
-
-        Set<MoveInterface> actualJumpMoves = mg.getJumpMoves(PieceColor.BLACK);
-        assertEquals(expectedJumpMoves, actualJumpMoves);
-
-        // expected white moves
-        expectedJumpMoves = new HashSet<>();
-        expectedJumpMoves.add(new SingleJump(13, 6, board));
-        expectedJumpMoves.add(new SingleJump(14, 5, board));
-        expectedJumpMoves.add(new SingleJump(14, 7, board));
-        expectedJumpMoves.add(new SingleJump(15, 6, board));
-        expectedJumpMoves.add(new SingleJump(15, 8, board));
-        expectedJumpMoves.add(new SingleJump(16, 7, board));
-
-        actualJumpMoves = mg.getJumpMoves(PieceColor.WHITE);
-        assertEquals(expectedJumpMoves, actualJumpMoves);
-    }
-
-    @Test
-    public void testDetermineNonJumpMoves_withAndWithoutKings() {
-
-        List<Integer> blackPositions = new ArrayList<>();
-        List<Integer> whitePositions = new ArrayList<>();
-
-        // board with just one black piece
-        Board board = new Board(blackPositions, whitePositions);
-        board.setOccupyingPiece(1, new Piece(PieceColor.BLACK));
-
-        MoveGenerator mg = new MoveGenerator(board);
-
-        // expected black moves
-        Set<MoveInterface> expectedNonJumpMovesSet = new HashSet<>();
-        expectedNonJumpMovesSet.add(new Move(1, 5, board));
-        expectedNonJumpMovesSet.add(new Move(1, 6, board));
-
-        Set<MoveInterface> actualNonJumpMovesSet = mg.getNonJumpMoves(PieceColor.BLACK);
-        assertEquals(expectedNonJumpMovesSet, actualNonJumpMovesSet);
-
-        // board with three white pieces, three black pieces, one black king
-        blackPositions = Arrays.asList(1, 4, 18);
-        whitePositions = Arrays.asList(12, 15, 29);
-        board = new Board(blackPositions, whitePositions);
-        board.getPiece(18).kingMe();
-
-        mg = new MoveGenerator(board);
-
-        // expected black moves
-        expectedNonJumpMovesSet = new HashSet<>();
-        expectedNonJumpMovesSet.add(new Move(1, 5, board));
-        expectedNonJumpMovesSet.add(new Move(1, 6, board));
-        expectedNonJumpMovesSet.add(new Move(4, 8, board));
-        expectedNonJumpMovesSet.add(new Move(18, 14, board));
-        expectedNonJumpMovesSet.add(new Move(18, 22, board));
-        expectedNonJumpMovesSet.add(new Move(18, 23, board));
-
-        actualNonJumpMovesSet = mg.getNonJumpMoves(PieceColor.BLACK);
-        assertEquals(expectedNonJumpMovesSet, actualNonJumpMovesSet);
-
-        // expected white moves
-        expectedNonJumpMovesSet = new HashSet<>();
-        expectedNonJumpMovesSet.add(new Move(12, 8, board));
-        expectedNonJumpMovesSet.add(new Move(15, 10, board));
-        expectedNonJumpMovesSet.add(new Move(15, 11, board));
-        expectedNonJumpMovesSet.add(new Move(29, 25, board));
-
-        actualNonJumpMovesSet = mg.getNonJumpMoves(PieceColor.WHITE);
-        assertEquals(expectedNonJumpMovesSet, actualNonJumpMovesSet);
-    }
-
-    @Test
     public void testGetAllMoves_withKings() {
         List<Integer> blackPositions = Arrays.asList(10, 11, 13, 18, 25, 27, 30);
         List<Integer> whitePositions = Arrays.asList(1, 7, 8, 16, 22, 23, 24);
@@ -209,5 +104,110 @@ public class MoveGeneratorTest {
 
         actualMoves = mg.getAllPossibleMoves(PieceColor.WHITE);
         assertEquals(expectedMoves, actualMoves);
+    }
+
+    @Test
+    public void testGetJumpMoves_MultiJumpsWithoutKings() {
+
+        List<Integer> blackPositions = Arrays.asList(6, 7, 8, 14, 15);
+        List<Integer> whitePositions = Arrays.asList(18);
+        Board board = new Board(blackPositions, whitePositions);
+
+        // expected white moves
+        Set<MoveInterface> expectedJumpMoves = new HashSet<>();
+        expectedJumpMoves.add(new MultiJump(18, 2, Arrays.asList(9), board));
+        expectedJumpMoves.add(new MultiJump(18, 2, Arrays.asList(11), board));
+        expectedJumpMoves.add(new MultiJump(18, 4, Arrays.asList(11), board));
+
+        PieceColor currenetPlayersColor = PieceColor.WHITE;
+        MoveGenerator mg = new MoveGenerator(board);
+
+        Set<MoveInterface> actualJumpMoves = mg.getJumpMoves(currenetPlayersColor);
+        assertEquals(expectedJumpMoves, actualJumpMoves);
+    }
+
+    @Test
+    public void testGetJumpMoves_withoutKings() {
+
+        List<Integer> blackPositions = Arrays.asList(9, 10, 11, 12);
+        List<Integer> whitePositions = Arrays.asList(13, 14, 15, 16);
+        Board board = new Board(blackPositions, whitePositions);
+
+        // expected black moves
+        Set<MoveInterface> expectedJumpMoves = new HashSet<>();
+        expectedJumpMoves.add(new SingleJump(9, 18, board));
+        expectedJumpMoves.add(new SingleJump(10, 17, board));
+        expectedJumpMoves.add(new SingleJump(10, 19, board));
+        expectedJumpMoves.add(new SingleJump(11, 18, board));
+        expectedJumpMoves.add(new SingleJump(11, 20, board));
+        expectedJumpMoves.add(new SingleJump(12, 19, board));
+
+        MoveGenerator mg = new MoveGenerator(board);
+
+        Set<MoveInterface> actualJumpMoves = mg.getJumpMoves(PieceColor.BLACK);
+        assertEquals(expectedJumpMoves, actualJumpMoves);
+
+        // expected white moves
+        expectedJumpMoves = new HashSet<>();
+        expectedJumpMoves.add(new SingleJump(13, 6, board));
+        expectedJumpMoves.add(new SingleJump(14, 5, board));
+        expectedJumpMoves.add(new SingleJump(14, 7, board));
+        expectedJumpMoves.add(new SingleJump(15, 6, board));
+        expectedJumpMoves.add(new SingleJump(15, 8, board));
+        expectedJumpMoves.add(new SingleJump(16, 7, board));
+
+        actualJumpMoves = mg.getJumpMoves(PieceColor.WHITE);
+        assertEquals(expectedJumpMoves, actualJumpMoves);
+    }
+
+    @Test
+    public void testGetNonJumpMoves_withAndWithoutKings() {
+
+        List<Integer> blackPositions = new ArrayList<>();
+        List<Integer> whitePositions = new ArrayList<>();
+
+        // board with just one black piece
+        Board board = new Board(blackPositions, whitePositions);
+        board.setOccupyingPiece(1, new Piece(PieceColor.BLACK));
+
+        MoveGenerator mg = new MoveGenerator(board);
+
+        // expected black moves
+        Set<MoveInterface> expectedNonJumpMovesSet = new HashSet<>();
+        expectedNonJumpMovesSet.add(new Move(1, 5, board));
+        expectedNonJumpMovesSet.add(new Move(1, 6, board));
+
+        Set<MoveInterface> actualNonJumpMovesSet = mg.getNonJumpMoves(PieceColor.BLACK);
+        assertEquals(expectedNonJumpMovesSet, actualNonJumpMovesSet);
+
+        // board with three white pieces, three black pieces, one black king
+        blackPositions = Arrays.asList(1, 4, 18);
+        whitePositions = Arrays.asList(12, 15, 29);
+        board = new Board(blackPositions, whitePositions);
+        board.getPiece(18).kingMe();
+
+        mg = new MoveGenerator(board);
+
+        // expected black moves
+        expectedNonJumpMovesSet = new HashSet<>();
+        expectedNonJumpMovesSet.add(new Move(1, 5, board));
+        expectedNonJumpMovesSet.add(new Move(1, 6, board));
+        expectedNonJumpMovesSet.add(new Move(4, 8, board));
+        expectedNonJumpMovesSet.add(new Move(18, 14, board));
+        expectedNonJumpMovesSet.add(new Move(18, 22, board));
+        expectedNonJumpMovesSet.add(new Move(18, 23, board));
+
+        actualNonJumpMovesSet = mg.getNonJumpMoves(PieceColor.BLACK);
+        assertEquals(expectedNonJumpMovesSet, actualNonJumpMovesSet);
+
+        // expected white moves
+        expectedNonJumpMovesSet = new HashSet<>();
+        expectedNonJumpMovesSet.add(new Move(12, 8, board));
+        expectedNonJumpMovesSet.add(new Move(15, 10, board));
+        expectedNonJumpMovesSet.add(new Move(15, 11, board));
+        expectedNonJumpMovesSet.add(new Move(29, 25, board));
+
+        actualNonJumpMovesSet = mg.getNonJumpMoves(PieceColor.WHITE);
+        assertEquals(expectedNonJumpMovesSet, actualNonJumpMovesSet);
     }
 }
