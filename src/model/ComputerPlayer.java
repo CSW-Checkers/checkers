f485894d426abd38dd3078b21e8b371269ea60ec
Stefano Prezioso <stefprez@gmail.com>
2015-10-14 00:42:53 -0400

    Remove unnecessary comment

--- src/model/ComputerPlayer.java
+++ src/model/ComputerPlayer.java
@@ -22,14 +22,6 @@ public class ComputerPlayer implements Player {
         currentBoard.movePiece(moveToMake);
         this.printMove(moveToMake);
     }
-    // @Override
-    // public void makeMove(Board currentBoard) {
-    // final Set<MoveInterface> possibleMoves = MoveGenerator.getAllPossibleMoves(currentBoard,
-    // this.getColor());
-    // final MoveInterface moveToMake = (MoveInterface) possibleMoves.toArray()[0];
-    // currentBoard.movePiece(moveToMake);
-    // this.printMove(moveToMake);
-    // }
 
     private void printMove(MoveInterface moveToMake) {
         System.out.println(this.color.toString() + ": " + moveToMake);
