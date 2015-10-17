0fe4a93227bff4ea98dca334e62e5f8e7bf67a04
Cade Sperlich <csperlich@gmail.com>
2015-10-17 17:09:36 -0400

    BackRowCountEvaluator now only iterates over positions 1-4 and 29-32 instead of whole board.

--- src/model/ai/evaluation/BackRowCountEvaluator.java
+++ src/model/ai/evaluation/BackRowCountEvaluator.java
@@ -2,8 +2,6 @@ package model.ai.evaluation;
 
 import model.Board;
 import model.PieceColor;
-import model.PieceInterface;
-import model.Square;
 
 public class BackRowCountEvaluator extends BoardEvaluator {
     public BackRowCountEvaluator() {
@@ -18,20 +16,22 @@ public class BackRowCountEvaluator extends BoardEvaluator {
     public double evaluateBoard(Board theBoard, PieceColor color) {
         double value = 0.0;
 
-        for (Square square : theBoard.getGameState()) {
-            PieceInterface piece = square.getOccupyingPiece();
+        for (int i = 1; i <= 4; i++) {
 
             // if black piece is on black edge of board
-            if (square.isOnBlackEdgeOfBoard() && piece.getColor() == PieceColor.BLACK) {
+            if (theBoard.getPiece(i).getColor() == PieceColor.BLACK) {
                 if (color == PieceColor.BLACK) {
                     value += 1.0;
                 } else {
                     value -= 1.0;
                 }
             }
+        }
+
+        for (int i = 29; i <= 32; i++) {
 
             // if white piece is on white edge of board
-            else if (square.isOnWhiteEdgeOfBoard() && piece.getColor() == PieceColor.WHITE) {
+            if (theBoard.getPiece(i).getColor() == PieceColor.WHITE) {
                 if (color == PieceColor.WHITE) {
                     value += 1.0;
                 } else {
