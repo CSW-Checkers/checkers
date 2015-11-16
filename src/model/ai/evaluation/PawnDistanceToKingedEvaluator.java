14460df1349dcf8f068d78e714cd670375f44f6a
Cade Sperlich <csperlich@gmail.com>
2015-11-16 00:59:28 -0500

    Add PawnDistanceToKingedEvaluator and corresponding test class

---
+++ src/model/ai/evaluation/PawnDistanceToKingedEvaluator.java
@@ -0,0 +1,54 @@
+package model.ai.evaluation;
+
+import model.Board;
+import model.PieceColor;
+import model.PieceInterface;
+import model.Square;
+
+public class PawnDistanceToKingedEvaluator implements BoardEvaluatorInterface {
+
+    private static BoardEvaluatorInterface instance = null;
+
+    public static BoardEvaluatorInterface getInstance() {
+        if (instance == null) {
+            return new PawnDistanceToKingedEvaluator();
+        }
+        return instance;
+    }
+
+    private PawnDistanceToKingedEvaluator() {
+    }
+
+    @Override
+    public double evaluateBoard(Board theBoard, PieceColor color) {
+        double score = 0.0;
+
+        for (final Square square : theBoard.getGameState()) {
+
+            if (square.isOccupied()) {
+                final PieceInterface piece = square.getOccupyingPiece();
+
+                if (!piece.isKing()) {
+                    final int rowNumber = square.getRowNumber();
+                    int distanceToOtherSide = 0;
+
+                    if (piece.getColor() == PieceColor.BLACK) {
+                        distanceToOtherSide = 8 - rowNumber;
+                    } else {
+                        distanceToOtherSide = rowNumber - 1;
+                    }
+
+                    final double value = 8 - distanceToOtherSide;
+                    if (piece.getColor() == color) {
+                        score += value;
+                    } else {
+                        score -= value;
+                    }
+                }
+            }
+        }
+
+        return score;
+    }
+
+}
